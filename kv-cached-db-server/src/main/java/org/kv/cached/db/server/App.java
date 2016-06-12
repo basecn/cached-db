75970f2c00eb2197aa055659d1624c93f9c47104
云公司-张经纬 <18611105756@189.cn>
2016-06-12 09:42:30 +0800

    Init project module

---
+++ kv-cached-db-server/src/main/java/org/kv/cached/db/server/App.java
@@ -0,0 +1,13 @@
+package org.kv.cached.db.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
